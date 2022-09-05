7d506108f68cdc4ca171f164b04a3ad7a7bc9520
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-09-05 10:25:18 -0300

    Render múltiplos componentes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Football from './Football';
+import Garage from './Garage';
 import MyForm from './MyForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<MyForm />);
+function renderTudo() {
+	return (
+		<>
+			<Football />
+			<Garage />
+			<MyForm />
+		</>
+	);
+};
+
+root.render(renderTudo());
