fd487c1569eaf43006ea114977aa254eea1a818e
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-09-05 09:47:28 -0300

    Carregar componente de forma condicional

--- src/Football.js
+++ src/Football.js
@@ -1,11 +1,32 @@
+function MadeGoal() {
+	return <h2>GOOOOL!!!</h2>
+}
+
+function MissedGoal() {
+	return <h2>ERROU!!!</h2>
+}
+
+function Goal(props) {
+	console.log(props.isGoal);
+	
+	return (
+		<>
+			{ props.isGoal ? <MadeGoal /> : <MissedGoal /> }
+		</>
+	);
+}
+
 function Football() {
-	const shoot = (a, b) => {
-		console.log(a);
-		console.log(b.type);
+	const shoot = (txt, evt) => {
+		console.log(txt);
+		console.log(evt.type);
 	}
 
 	return (
-		<button onClick={(event) => shoot("Olá mundo!", event)}>Take the shot!</button>
+		<>
+			<button onClick={ (event) => shoot("Olá mundo!", event) }>Take the shot!</button>
+			<Goal isGoal={ true } />
+		</>
 	);
 }
 
