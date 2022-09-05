41209c8ecb001e14845d50eab63a6d790768fdce
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-09-05 09:52:52 -0300

    Usando listas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Football from './Football';
+import Garage from './Garage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Football />);
+
+root.render(<Garage />);
